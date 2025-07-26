8838c8e34ebeddf47d8aa40b9b0f8985eaa85af2
sergiomerces <sergio.merces@gmail.com>
2025-07-26 02:23:04 -0300

    configurando o prompt e aplicando chalk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import prompt from 'prompt';
+import mainPrompt from './prompt/prompt-main.js';
 
 async function main() {
-    
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log('Escolheu o QRCode');
+        if(choose.select == 2) console.log('Escolheu o Password');
+    });
+
+    prompt.start();
 }
 
 main();
